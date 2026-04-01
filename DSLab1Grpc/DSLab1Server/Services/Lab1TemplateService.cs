8d6008836c46e7a84845c7b883e52d04c98cf786
Lazar Krstic <7krle7@gmail.com>
2026-04-01 02:54:44 +0200

    Gitignore izmenjen

---
+++ DSLab1Grpc/DSLab1Server/Services/Lab1TemplateService.cs
@@ -0,0 +1,15 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace DSLab1Server.Services;
+
+public class Lab1TemplateService : Lab1Template.Lab1TemplateBase
+{
+    public override Task<TextReply> Ping(Empty request, ServerCallContext context)
+    {
+        return Task.FromResult(new TextReply
+        {
+            Text = "Server radi"
+        });
+    }
+}
